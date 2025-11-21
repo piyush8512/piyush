d9db633134dde4a98a0d7d5b2d86125865dd4e2e
piyush8512 <prajapatipiyush851@gmail.com>
2025-11-21 10:06:33 +0530

    cors problme solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 import Intro from "@/components/home/Intro";
-// import { Seperator } from "@/components/Section";
+import { Seperator } from "@/components/Section";
 import Projects from "@/components/home/Projects";
-// import Skills from "@/components/home/Skills";
-// import About from "@/components/home/About";
-// import Contact from "@/components/home/Contact";
-// import Blog from "@/components/home/Blog";
+import Skills from "@/components/home/Skills";
+import About from "@/components/home/About";
+import Contact from "@/components/home/Contact";
+import Blog from "@/components/home/Blog";
 
 export default async function Home() {
   return (
     <>
       <Intro />
-     <Projects home />
-        {/*<Seperator />
+      <Projects home />
+      <Seperator />
       <Blog home />
       <Seperator />
       <Skills home />
       <Seperator />
       <About home />
       <Seperator />
-      <Contact home /> */}
+      <Contact home />
     </>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,8 @@
-import { Seperator } from "@/components/Section";
-import AIProjects from "@/components/AiProjects";
+// import { Seperator } from "@/components/Section";
+// import AIProjects from "@/components/AiProjects";
 import Projects from "@/components/CompletedProjects";
-import OtherProjects from "@/components/OtherProject";
-import UtilProjects from "@/components/UtilProjects";
+// import OtherProjects from "@/components/OtherProject";
+// import UtilProjects from "@/components/UtilProjects";
 export const metadata = {
   title: "Piyush Projects | Software Engineer & Web Developer",
   description:
@@ -13,12 +13,12 @@ export default function ProjectsPage() {
   return (
     <>
       <Projects />
-      <Seperator />
+      {/* <Seperator />
       <AIProjects />
       <Seperator />
       <UtilProjects />
       <Seperator />
-      <OtherProjects />
+      <OtherProjects /> */}
     </>
   );
 }
